a4fdbef57966dfcb3eafac7bab8f61b875f8638c
AryanMir15 <aryanmir115@gmail.com>
2025-08-13 17:58:18 +0500

    another fix

--- controllers/paddleController.js
+++ controllers/paddleController.js
@@ -137,9 +137,9 @@ const createCheckoutSession = async (req, res) => {
           userId: user._id.toString(),
           productType: productType,
         },
-        successUrl: `${process.env.CLIENT_URL}/checkout?payment=success`,
-        cancelUrl: `${process.env.CLIENT_URL}/checkout?payment=cancelled`,
-        returnUrl: `${process.env.CLIENT_URL}/checkout`,
+        successUrl: `${process.env.CLIENT_URL}/dashboard?payment=success`,
+        cancelUrl: `${process.env.CLIENT_URL}/pricing?payment=cancelled`,
+        returnUrl: `${process.env.CLIENT_URL}/dashboard`,
       });
 
       console.log("Transaction created successfully:", transaction.id);
@@ -193,9 +193,9 @@ const createCheckoutSession = async (req, res) => {
               userId: user._id.toString(),
               productType: productType,
             },
-            successUrl: `${process.env.CLIENT_URL}/checkout?payment=success`,
-            cancelUrl: `${process.env.CLIENT_URL}/checkout?payment=cancelled`,
-            returnUrl: `${process.env.CLIENT_URL}/checkout`,
+            successUrl: `${process.env.CLIENT_URL}/dashboard?payment=success`,
+            cancelUrl: `${process.env.CLIENT_URL}/pricing?payment=cancelled`,
+            returnUrl: `${process.env.CLIENT_URL}/dashboard`,
           });
 
           console.log(
