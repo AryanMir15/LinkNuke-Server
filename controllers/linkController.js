c4be0082bf1c2e43ba20586535b9f8da1d066ec1
AryanMir15 <aryanmir115@gmail.com>
2025-08-23 12:26:59 +0500

    some dumbass error

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -160,6 +160,9 @@ const getAllLinks = async (req, res) => {
   }
 };
 
+const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
 // Get usage statistics
 const getUsageStats = asyncHandler(async (req, res) => {
   const startOfMonth = new Date();
@@ -290,4 +293,5 @@ module.exports = {
   getLinkByLinkId,
   createCheckoutSession,
   handleWebhook,
+  getUsageStats,
 };
