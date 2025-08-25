8a1b4e295e8d665f260cbc3bfaaea7491987069a
AryanMir15 <aryanmir115@gmail.com>
2025-08-25 19:27:01 +0500

    jwt gone

--- app.js
+++ app.js
@@ -59,7 +59,8 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24,
     },
   })
 );
